f9926691fd1266d54c697401385489a2c864e9d2
seol-wonjun <qiops@naver.com>
2024-01-25 16:27:17 +0900

    update WebNavbar

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,9 +1,11 @@
 import React from "react";
 import WebMain from "../Components/WebMain";
+import WebNavbar from "../Components/WebNavbar";
 
 const Main = () => {
   return (
     <div>
+      <WebNavbar />
       <WebMain />
     </div>
   );
