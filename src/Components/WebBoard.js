26f8e4402788e7e48ce517ef10eb6394494b2c85
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-25 22:29:56 +0900

    Kye (#9)
    * 메인페이지 디자인
    
    * git ignore에 env 추가
    
    * 수정 요청사항 반영

--- src/Components/WebBoard.js
+++ src/Components/WebBoard.js
@@ -8,10 +8,10 @@ import { getBoard } from "../Api/Api";
 const Board = () => {
   const [boardInfo, setBoardInfo] = useState([]);
   const columns = [
-    { key: "id", label: "" },
+    { key: "id", label: "id" },
     { key: "title", label: "제목" },
     { key: "author", label: "작성자" },
-    { key: "content", label: "내용" },
+    { key: "content", label: "내용", hidden: true },
     { key: "createdAt", label: "작성일" },
   ];
   useEffect(() => {
@@ -19,6 +19,9 @@ const Board = () => {
       try {
         const response = await getBoard();
         if (response && response.boardResponseList) {
+          const sortedBoardInfo = response.boardResponseList.sort(
+            (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+          );
           setBoardInfo(response.boardResponseList);
         } else {
           alert("게시글이 없습니다.");
@@ -31,41 +34,55 @@ const Board = () => {
     fetchData();
   }, []);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}/${month}/${day}`;
+  };
+
   return (
     <>
       <GlobalStyle />
-        <StyledTable>
-          <thead>
-            <tr>
-
-              {columns.map((column) => (
-                <th key={column.key}>{column.label}</th>
-              ))}
+      <StyledTable>
+        <thead>
+          <tr>
+            {columns.map(
+              (column) =>
+                !column.hidden && <th key={column.key}>{column.label}</th>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {boardInfo.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map(
+                (column) =>
+                  !column.hidden &&
+                  column.key !== "content" && (
+                    <td key={column.key}>
+                      {column.key === "createdAt"
+                        ? formatDate(row[column.key])
+                        : row[column.key]}
+                    </td>
+                  )
+              )}
             </tr>
-          </thead>
-          <tbody>
-            {boardInfo.map((row, rowIndex) => (
-              <tr key={rowIndex}>
-                {columns.map((column) => (
-                  <td key={column.key}>{row[column.key]}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </StyledTable>
+          ))}
+        </tbody>
+      </StyledTable>
     </>
   );
 };
-
-const StyledTable = styled.table`
-  /* 필요한 스타일 추가 */
-`;
-
 const GlobalStyle = createGlobalStyle`
   body {
     margin : 0;
     padding: 0;
   }
+`;
+const StyledTable = styled.table`
+  /* 필요한 스타일 추가 */
 `;
 
 export default Board;
