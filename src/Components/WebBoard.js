51b3cda2f7de0814ace753c80f59b887fa084437
seol-wonjun <qiops@naver.com>
2024-01-27 11:26:32 +0900

    create pagenation viewCnt

--- src/Components/WebBoard.js
+++ src/Components/WebBoard.js
@@ -8,13 +8,17 @@ import { getAllBoards } from "../Api/Api";
 const Board = () => {
   const [boardInfo, setBoardInfo] = useState([]);
   const navigate = useNavigate();
+  const itemsPerPage = 3; // 한 페이지당 표시할 게시물 수
+  const [currentPage, setCurrentPage] = useState(1);
   const columns = [
     { key: "id", label: "id" },
     { key: "title", label: "제목" },
     { key: "author", label: "작성자" },
     { key: "content", label: "내용", hidden: true },
     { key: "createdAt", label: "작성일" },
+    { key: "viewCnt", label: "조회수" },
   ];
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -47,6 +51,38 @@ const Board = () => {
     navigate(`/board/${postId}`);
   };
 
+  const calculatePageInfo = () => { // 현재 페이지와 전체 페이지 수를 계산하는 함수
+    const totalPages = Math.ceil(boardInfo.length / itemsPerPage);
+    return { totalPages };
+  };
+
+  const { totalPages } = calculatePageInfo();
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  
+  const getVisibleItems = () => { // 현재 페이지에 따라 표시할 게시물을 필터링하는 함수
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return boardInfo.slice(startIndex, endIndex);
+  };
+
+  const visibleItems = getVisibleItems();
+
+  const Pagination = () => (
+    <StyledPagination>
+      <button onClick={() => handlePageClick(1)}>첫 페이지</button>
+      {Array.from({ length: totalPages }).map((_, index) => (
+        <button key={index + 1} onClick={() => handlePageClick(index + 1)}>
+          {index + 1}
+        </button>
+      ))}
+      <button onClick={() => handlePageClick(totalPages)}>끝 페이지</button>
+    </StyledPagination>
+  );
+
   return (
     <>
       <GlobalStyle />
@@ -60,27 +96,28 @@ const Board = () => {
           </tr>
         </thead>
         <tbody>
-          {boardInfo.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {columns.map(
-                (column) =>
-                  !column.hidden &&
-                  column.key !== "content" && (
-                    <td key={column.key}
-                      onClick={() =>
-                        column.key === "title" && handleTitleClick(row["id"])
-                      }
-                    >
-                      {column.key === "createdAt"
-                        ? formatDate(row[column.key])
-                        : row[column.key]}
-                    </td>
-                  )
-              )}
-            </tr>
-          ))}
+          {visibleItems.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                  {columns.map(
+                    (column) =>
+                      !column.hidden &&
+                      column.key !== "content" && (
+                        <td key={column.key}
+                          onClick={() =>
+                            column.key === "title" && handleTitleClick(row["id"])
+                          }
+                        >
+                          {column.key === "createdAt"
+                            ? formatDate(row[column.key])
+                            : row[column.key]}
+                        </td>
+                      )
+                  )}
+              </tr>
+            ))}
         </tbody>
       </StyledTable>
+      <Pagination />
     </>
   );
 };
@@ -90,9 +127,16 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 `;
+
 const StyledTable = styled.table`
   td {
     cursor: pointer; /* 제목에 커서를 갖다 대면 클릭하는 모션이 되도록 */
+  }
+`;
+
+const StyledPagination = styled.div`
+  button {
+    margin-right: 5px;
   }
 `;
 
